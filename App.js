ca66ee853b349176abcb204a99d71cb5a6ed79f1
TKreateDev <tanbir98@hotmail.it>
2022-12-19 17:08:29 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NativeBaseProvider, Box, Center, VStack } from "native-base";
+
+function HomeScreen() {
+  return (
+    <NativeBaseProvider>
+      <Box flex={1} alignItems={"center"} justifyContent={"center"}>
+        <VStack flex={1} alignItems={'center'} justifyContent={"center"}>
+          <Box>Hello world</Box>
+          <Box>Hello world</Box>
+          <Box>Hello world</Box>
+        </VStack>
+      </Box>
+    </NativeBaseProvider>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
---
+++ ethics.js
@@ -0,0 +1,142 @@
+const ethics =[
+    {
+        topic:'Ethics',
+        subtopics: [
+            {
+                title: 'Commitment And Accountability',
+                questions: [
+                    {
+                        level: '1: Starting',
+                        statement: 'Written senior management commitment to create a culture of integrity',
+                    },
+                    {
+                        level: '2: Developing',
+                        statement: 'Management responsibility has been allocated and a person for ethics related topics assigned',
+                    },
+                    {
+                        level: '2: Developing',
+                        statement: 'Whistle blowing or anonymous system to report on unethical practices and violations',
+                    },
+                    {
+                        level: '3: Implementing',
+                        statement: 'Include the ethics compliance requirements as integral part of the contract & agreements with 3rd parties',
+                    },
+                    {
+                        level: '3: Implementing',
+                        statement: 'Report regularly to the Board / committee',
+                    },
+                    {
+                        level: '3: Implementing',
+                        statement: 'Develop a formal reporting mechanism',
+                    },
+                    {
+                        level: '4: Leading',
+                        statement: 'Ethics and Compliance culture that is measured through surveys and encouraging employees to speak up and report concerns',
+                    },
+                    {
+                        level: '4: Leading',
+                        questions: 'Partner with expert organization for due diligence',
+                    },
+                ],
+            },
+            {
+                title: 'Legal & Customer Requirements',
+                questions: [
+                    {
+                        level: '1: Starting',
+                        statement: 'Compliance with applicable laws and regulations for the Ethics elements (PSCI definition): - Anti-Bribery and Corruption - Fair Competition - Animal Welfare - Data Privacy and Security - Patient Safety and Access to Information - Conflict of Interest',
+                    },
+                    {
+                        level: '2: Developing',
+                        statement: 'Develop testing and monitoring programs for elements in Ethics',
+                    },
+                    {
+                        level: '2: Developing',
+                        statement: 'Ethics aspect added to the internal audit agenda',
+                    },
+                    {
+                        level: '3: Implementing',
+                        statement: 'Mechanism in anticipation of new legislations',
+                    },
+                    {
+                        level: '4: Leading',
+                        statement: 'Benchmark Ethics program, best practices + implement learnings',
+                    },
+                ],
+            },
+            {
+                title: 'Policy Commitment',
+                questions: [
+                    {
+                        level: '1: Starting',
+                        statement: 'Policies and Procedures covering the Ethics elements ​ in a written document that meet the minimum standards (for example Code of Conduct)',
+                    },
+                    {
+                        level: '1: Starting',
+                        statement: 'Policy on protection of whistle blowers and anonymous reporting',
+                    },
+                    {
+                        level: '2: Developing',
+                        statement: 'Practices to manage investigation of employee concerns developed',
+                    },
+                    {
+                        level: '2: Developing',
+                        statement: 'Exhibit the company policy and approach on company website and prominent locations',
+                    },
+                    {
+                        level: '3: Implementing',
+                        statement: 'Implement a supplier Code of Conduct that describes expectations on 3rd parties regarding the key elements of Ethics',
+                    },
+                    {
+                        level: '4: Leading',
+                        statement: 'Support 3rd parties as they develop their ethics and compliance culture to ensure a responsible supply chain',
+                    },
+                ],
+            },
+            {
+                title: 'Risk Management',
+                questions: [
+                    {
+                        level: '1: Starting',
+                        statement: 'Build framework and methodology for managing Ethics risks',
+                    },
+                    {
+                        level: '2: Developing',
+                        statement: 'Key Ethic risks identified and assessed',
+                    },
+                    {
+                        level: '3: Implementing',
+                        statement: 'A robust framework and methodology for managing Ethics risks implemented',
+                    },
+                    {
+                        level: '4: Leading',
+                        statement: 'Framework for risk management includes review of the whole value chain',
+                    },
+                ],
+            },
+            {
+                title: 'Training',
+                questions: [
+                    {
+                        level: '1: Starting',
+                        questions: 'Start developing specific training and awareness program for target positions',
+                    },
+                    {
+                        level: '2: Developing',
+                        questions: 'Key functions and positions crucial for an ethical business culture identified (e.g. compliance, sourcing, Sales & Mktg, HR and executive position) for mandatory training and declaration',
+                    },
+                    {
+                        level: '3: Implementing',
+                        questions: 'Yearly awareness training in the ethics element mandatory for all employees',
+                    },
+                    {
+                        level: '4: Leading',
+                        questions: '(Coming Soon)',
+                    },
+                ],
+            },
+        ]
+    }
+]
+
+export default ethics;
